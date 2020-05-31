2a88abe6b93ecd3409a1037b0aac158119b6ee03
kidevelop <hjh010501@naver.com>
2020-06-01 01:10:03 +0900

    (add): naver maps

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { RenderAfterNavermapsLoaded, NaverMap } from 'react-naver-maps'; // 패키지 불러오기
+
+function NaverMapAPI() {
+  return (
+    <NaverMap
+      mapDivId={'maps-getting-started-uncontrolled'}
+      style={{
+        width: '100%',
+        height: '100vh'
+      }}
+      defaultCenter={{ lat: 36.3715433, lng: 127.361 }}
+      defaultZoom={16}
+    />
+  );
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RenderAfterNavermapsLoaded
+      ncpClientId={'daz0zenz61'} // 자신의 네이버 계정에서 발급받은 Client ID
+      error={<p>Maps Load Error</p>}
+      loading={<p>Maps Loading...</p>}
+    >
+      <NaverMapAPI />
+    </RenderAfterNavermapsLoaded>
   );
 }
 
